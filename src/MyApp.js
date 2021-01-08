6c99d53e5c60fd495d432be6ee847948b525444a
MaggieYang57 <myang57@calpoly.edu>
2021-01-08 15:30:39 -0800

    Final Commit

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,27 +1,15 @@
 import React, {useState} from 'react';
 import Table from './Table'
+import Form from './Form';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([  
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
- function removeOneCharacter (index) {
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
        return i !== index
     });
@@ -31,8 +19,9 @@ function MyApp() {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
-  ) 
+  )
 }
 
 export default MyApp;
